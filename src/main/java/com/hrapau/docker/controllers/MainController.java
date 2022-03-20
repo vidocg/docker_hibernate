d9eccffef9aaf404ce47bf124c1038af0b0ceb1a
Viktar Hrapau <Viktar.Hrapau@itechart-group.com>
2022-03-20 20:51:46 +0300

    Init commit [dockerfile and controller]

---
+++ src/main/java/com/hrapau/docker/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.hrapau.docker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MainController {
+    @GetMapping("/all")
+    public List<String> getItemsIds() {
+        return Arrays.asList("firs", "second", "third");
+    }
+}
